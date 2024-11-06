e582308fad7cc1daa8087742d9fd272a9185c618
Nur Ahmed <06nurahmed@gmail.com>
2024-11-06 22:31:36 +0600

    pixomedium

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
+import Skills from "@/components/main/Skills";
+import Service from "@/components/main/Service";
+import ProjectDetailTab from "@/components/sub/Projectdetailtab";
+import TeamDetails from "@/components/sub/TeamDetails";
+import Compare from "@/components/sub/compare";
+import Pricing from "@/components/sub/Pricing";
+import Add from "@/components/sub/Add";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        <ProjectDetailTab />
+        <Compare />
+        <TeamDetails />
+        <Skills />
+        <Encryption />
+        <Service />
+        <Projects />
+        <Pricing />
+        <Add />
+      </div>
+    </main>
+  );
+}
